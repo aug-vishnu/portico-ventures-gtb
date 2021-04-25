2203b8a8dd0075b2ed26f7df9a113a7d15240533
aug-vishnu <vishnuprabhu.bvk@gmail.com>
2021-04-25 20:20:16 +0530

    Added static files

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import content from "../../site/content/contact.json"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -15,7 +16,6 @@ const BlogIndex = ({ data, location }) => {
         <Seo title="All posts" />
         <Bio />
         <p>
-          {/* {content.content} */}
           No blog posts found. Add markdown posts to "content/blog" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
           gatsby-config.js).
@@ -23,22 +23,23 @@ const BlogIndex = ({ data, location }) => {
       </Layout>
     )
   }
-
+  
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="All posts" />
       <Bio />
+      {content.content}
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
-
+          
           return (
             <li key={post.fields.slug}>
               <article
                 className="post-list-item"
                 itemScope
                 itemType="http://schema.org/Article"
-              >
+                >
                 <header>
                   <h2>
                     <Link to={post.fields.slug} itemProp="url">
@@ -53,7 +54,7 @@ const BlogIndex = ({ data, location }) => {
                       __html: post.frontmatter.description || post.excerpt,
                     }}
                     itemProp="description"
-                  />
+                    />
                 </section>
               </article>
             </li>
